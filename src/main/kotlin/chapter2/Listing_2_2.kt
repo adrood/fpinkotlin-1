5d71332192beb80a1c5e1ac7c5805ab846cd6152
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-03-15 22:31:43 +0100

    Comments added.

--- src/main/kotlin/chapter2/Listing_2_2.kt
+++ src/main/kotlin/chapter2/Listing_2_2.kt
@@ -4,9 +4,11 @@ object Listing_2_2 {
 
     //tag::factorial[]
     fun factorial(i: Int): Int {
+        // An inner or local function definition
         fun go(n: Int, acc: Int): Int = // <1>
             if (n <= 0) acc
             else go(n - 1, n * acc)
+        // Calling the local function.
         return go(i, 1) // <2>
     }
     //end::factorial[]
@@ -16,9 +18,12 @@ object Listing_2_2_1 {
 
     //tag::factorial2[]
     fun factorial(i: Int): Int {
+        // The tailrec modifier instructs the compiler to
+        // eliminate tail calls
         tailrec fun go(n: Int, acc: Int): Int = // <1>
             if (n <= 0) acc
             else go(n - 1, n * acc) // <2>
+        // The function's final declaration(?) is in tail position
         return go(i, 1)
     }
     //end::factorial2[]
--- src/main/kotlin/chapter2/Listing_2_3.kt
+++ src/main/kotlin/chapter2/Listing_2_3.kt
@@ -9,6 +9,7 @@ object Listing_2_3 {
             if (n < 0) -n
             else n
 
+        // Add the factorial function, making it private
         private fun factorial(i: Int): Int { //<1>
             fun go(n: Int, acc: Int): Int =
                 if (n <= 0) acc
@@ -21,6 +22,7 @@ object Listing_2_3 {
             return msg.format(x, abs(x))
         }
 
+        // Add the formatFactorial function, public by default
         fun formatFactorial(x: Int): String { //<2>
             val msg = "The factorial of %d is %d"
             return msg.format(x, factorial(x))
@@ -29,6 +31,7 @@ object Listing_2_3 {
 
     fun main() {
         println(Example.formatAbs(-42))
+        // Call formatFactorial from the main function
         println(Example.formatFactorial(7)) //<3>
     }
     //end::init[]
