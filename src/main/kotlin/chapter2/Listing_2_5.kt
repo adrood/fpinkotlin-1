49732824e5e4d19730948327f7fb68d4be7de64c
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-03-20 13:21:50 +0100

    Comments added.

--- src/main/kotlin/chapter2/Listing_2_5.kt
+++ src/main/kotlin/chapter2/Listing_2_5.kt
@@ -2,10 +2,14 @@ package chapter2
 
 object Listing_2_5 {
     //tag::init[]
+    // Polymorphic function to find an element in an array.
+    // Operate on an array of A, take a predicate function operating
+    // on individual elements of A.
     fun <A> findFirst(xs: Array<A>, p: (A) -> Boolean): Int { // <1>
         tailrec fun loop(n: Int): Int =
             when {
                 n >= xs.size -> -1
+                // Apply the predicate function to the array element.
                 p(xs[n]) -> n // <2>
                 else -> loop(n + 1)
             }
