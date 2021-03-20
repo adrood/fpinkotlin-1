1131d6b9bbce20148ac84c61e30f72b16c953f11
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-03-20 13:22:01 +0100

    Comments added.

--- src/main/kotlin/chapter2/Listing_2_5_1.kt
+++ src/main/kotlin/chapter2/Listing_2_5_1.kt
@@ -2,10 +2,13 @@ package chapter2
 
 object Listing_2_5_1 {
     //tag::init[]
+    // Operate on an array of A, take a predicate function operating
+    // on individual elements of A
     fun <A> findFirst(xs: Array<A>, p: (A) -> Boolean): Int {
         tailrec fun loop(n: Int): Int =
             when { // <1>
                 n >= xs.size -> -1 // <2>
+                // Apply the predicate to the array element
                 p(xs[n]) -> n
                 else -> loop(n + 1) // <3>
             }
