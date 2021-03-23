0a83afd8864f2cf5aec014e7b91ee420e25a216e
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-03-23 13:34:31 +0100

    Changed function name in order to hint to an implementation using foldLeft.

--- src/test/kotlin/chapter3/exercises/Exercise_3_11.kt
+++ src/test/kotlin/chapter3/exercises/Exercise_3_11.kt
@@ -5,13 +5,13 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 
 // tag::init[]
-fun <A> reverse(xs: List<A>): List<A> = TODO()
+fun <A> reverseL(xs: List<A>): List<A> = TODO()
 // end::init[]
 
 class Exercise_3_11 : WordSpec({
     "list reverse" should {
         "!reverse list elements" {
-            reverse(List.of(1, 2, 3, 4, 5)) shouldBe
+            reverseL(List.of(1, 2, 3, 4, 5)) shouldBe
                 List.of(5, 4, 3, 2, 1)
         }
     }
