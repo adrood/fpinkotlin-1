2ff43d3c3540da77831f7c45798675108add91a9
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-03-31 08:45:17 +0200

    Comments added.

--- src/main/kotlin/chapter8/sec1/listing.kt
+++ src/main/kotlin/chapter8/sec1/listing.kt
@@ -7,13 +7,20 @@ import io.kotlintest.properties.forAll
 
 fun main() {
     //tag::init1[]
+    // A generator of lists containing integers between 0 and 100
     val intList = Gen.list(Gen.choose(0, 100)) // <1>
 
+    // A valid property that specifies the behavior of the
+    // List.reversed method
     forAll(intList) { // <2>
+        // Check that reversing a list twice gives back the original list
         (it.reversed().reversed() == it) and // <3>
-                (it.firstOption() == it.reversed().lastOrNone()) // <4>
+            // Check that the first element becomes the last
+            // element after reversal
+            (it.firstOption() == it.reversed().lastOrNone()) // <4>
     }
 
+    // Second property that fails under most conditions
     forAll(intList) { // <5>
         it.reversed() == it
     }
