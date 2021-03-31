ff9a9b968f3b8cda7782be3e153bd4c885d7f255
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-03-31 15:58:23 +0200

    Comments added.

--- src/main/kotlin/chapter14/sec1/listing.kt
+++ src/main/kotlin/chapter14/sec1/listing.kt
@@ -5,12 +5,15 @@ fun quicksort(xs: List<Int>): List<Int> =
     if (xs.isEmpty()) xs else {
         val arr = xs.toIntArray()
 
+        // swaps two elements in an array
         fun swap(x: Int, y: Int) { // <1>
             val tmp = arr[x]
             arr[x] = arr[y]
             arr[y] = tmp
         }
 
+        // Partitions a portion of the array into elements
+        // less than and greater than pivot respectively
         fun partition(n: Int, r: Int, pivot: Int): Int { // <2>
             val pivotVal = arr[pivot]
             swap(pivot, r)
@@ -23,6 +26,7 @@ fun quicksort(xs: List<Int>): List<Int> =
             return j
         }
 
+        // Sorts a portion of the array in place
         fun qs(n: Int, r: Int): Unit = if (n < r) { // <3>
             val pi = partition(n, r, n + (n - r) / 2)
             qs(n, pi - 1)
