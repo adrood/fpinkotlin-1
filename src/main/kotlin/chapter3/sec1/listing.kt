da5ea1d35d9ea31d84a013e58adffb47f3997ea3
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-02 19:28:56 +0200

    Comments added.

--- src/main/kotlin/chapter3/sec1/listing.kt
+++ src/main/kotlin/chapter3/sec1/listing.kt
@@ -1,9 +1,12 @@
 package chapter3.sec1
 
 //tag::init[]
+// Sealed definition of data type
 sealed class List<out A> // <1>
 
+// The Nil implementation of List
 object Nil : List<Nothing>() // <2>
 
+// The Cons implementation of List
 data class Cons<out A>(val head: A, val tail: List<A>) : List<A>() // <3>
 //end::init[]
