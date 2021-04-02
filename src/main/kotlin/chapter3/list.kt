46a7df4a848609481ae08afd6ed334f3bcdf9123
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-02 11:29:18 +0200

    Fixed error resulting from merge

--- src/main/kotlin/chapter3/list.kt
+++ src/main/kotlin/chapter3/list.kt
@@ -4,18 +4,6 @@ package chapter3
 
 //tag::example[]
 // Sealed definition of data type
-sealed class List<out A> { // <1>
-    //tag::comment[]
-    // helper functions
-    //end::comment[]
-    //tag::companion[]
-
-    // Companion object containing functions
-    companion object { // <2>
-
-        //tag::of[]
-        // Factory helper function
-        fun <A> of(vararg aa: A): List<A> { // <3>
 sealed class List<out A> {
     companion object {
         fun <A> of(vararg aa: A): List<A> {
