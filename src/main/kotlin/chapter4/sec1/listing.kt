5e682a6f0701fdf4b58255f083275e64348120ad
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-02 13:42:54 +0200

    Comments added.

--- src/main/kotlin/chapter4/sec1/listing.kt
+++ src/main/kotlin/chapter4/sec1/listing.kt
@@ -2,11 +2,13 @@ package chapter4.sec1
 
 //tag::init1[]
 fun failingFn(i: Int): Int {
+    // Declaration of type Int throws Exception
     val y: Int = throw Exception("boom") // <1>
     return try {
         val x = 42 + 5
         x + y
     } catch (e: Exception) {
+        // Unreachable code, so not returning 43
         43 // <2>
     }
 }
@@ -16,8 +18,11 @@ fun failingFn(i: Int): Int {
 fun failingFn2(i: Int): Int =
     try {
         val x = 42 + 5
+        // A thrown Exception can be annotated with any type,
+        // here it is Int
         x + (throw Exception("boom!")) as Int // <1>
     } catch (e: Exception) {
+        // Exception is caught, so returning 43
         43 // <2>
     }
 //end::init2[]
