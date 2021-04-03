7bc5f14b25333580878a82eafcd4c3e5bfea5ce8
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-03 13:33:35 +0200

    Comments added.

--- src/main/kotlin/chapter8/sec4_10/listing.kt
+++ src/main/kotlin/chapter8/sec4_10/listing.kt
@@ -16,16 +16,20 @@ fun <A, B, C> map2(ga: Gen<A>, gb: Gen<B>, f: (A, B) -> C): Gen<C> =
     TODO()
 
 //tag::init1[]
+// A weighted generator of executor services
 val ges: Gen<ExecutorService> = weighted( // <1>
     Gen.choose(1, 4).map {
+        // Create a fixed thread pool 75% of the time
         Executors.newFixedThreadPool(it)
     } to .75, // <2>
     Gen.unit(
+        // Create an unbounded thread pool 25% of the time
         Executors.newCachedThreadPool()
     ) to .25) // <3>
 
 fun <A> forAllPar(ga: Gen<A>, f: (A) -> Par<Boolean>): Prop =
     forAll(
+        // Create a Pair<Gen<ExecutorService>,Gen<A>> using the to keyword
         map2(ges, ga) { es, a -> es to a } // <4>
     ) { (es, a) -> f(a)(es).get() }
 //end::init1[]
