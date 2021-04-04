6a531b140442a383908dc48bd3d9ee4f8012ecc0
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-04 19:04:10 +0200

    Added imports required for solution implementation

--- src/test/kotlin/chapter5/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter5/exercises/ex1/listing.kt
@@ -2,9 +2,13 @@ package chapter5.exercises.ex1
 
 import chapter3.List
 import chapter5.Stream
+import chapter5.Cons
+import chapter5.Empty
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import utils.SOLUTION_HERE
+import chapter3.Cons as ConsL
+import chapter3.Nil as NilL
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise1 : WordSpec({
