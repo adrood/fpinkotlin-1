44943c2d24f598f1a9b04b79b059c2e2aabffdff
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-08 13:01:29 +0200

    Worksheet for examining and evaluating code fragments.

---
+++ src/main/kotlin/chapter10/ch10.ws.kts
@@ -0,0 +1,8 @@
+import arrow.core.extensions.list.foldable.foldLeft
+import chapter10.sec1.stringMonoid
+
+val words = listOf<String>("Hic", "Est", "Index")
+
+words.foldRight(stringMonoid.nil, stringMonoid::combine)
+
+words.foldLeft(stringMonoid.nil, stringMonoid::combine)
