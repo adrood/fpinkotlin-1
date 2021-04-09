242f8cd2d35f83ee714f4ee4a5f8013a5d3a39cb
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-09 11:38:59 +0200

    Added examples.

--- src/main/kotlin/chapter15/chapter15.ws.kts
+++ src/main/kotlin/chapter15/chapter15.ws.kts
@@ -1,5 +1,8 @@
+import chapter15.sec2.filter
 import chapter15.sec2.liftOne
+import chapter15.sec2.sum
 import chapter15.sec2.toList
+import chapter15.sec3_3.Process.Companion.lift
 import chapter5.Stream
 
 val p = liftOne<Int, Int> { it * 2  }
@@ -7,3 +10,14 @@ p(Stream.of(1, 2, 3, 4, 5)).toList()
 
 // As we can see, this Process just waits for one element,
 // emits it, and then stops.
+
+val units = Stream.continually(Unit)
+units
+
+// lift<Unit, Int> { _ -> 1 } (units)
+
+val even = filter<Int> {it % 2 == 0 }
+even(Stream.of(1, 2, 3, 4, 5)).toList()
+
+sum()(Stream.of(1.0, 2.0, 3.0, 4.0)).toList()
+
