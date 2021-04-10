ead3770b02981836d3f20ceb6d63fb591c6a6308
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-10 14:22:49 +0200

    Initial version.

---
+++ src/main/kotlin/chapter9/chapter9.ws.kts
@@ -0,0 +1,10 @@
+// import chapter9.Parser
+// import chapter9.sec1_1.Parsers
+// import chapter9.sec2_1.Parsers.map
+//
+// Section 9.1.1
+// fun char(c: Char): Parser<Char> = TODO()
+// char('a')
+// val c: Char = 'a';
+//
+// map(many(char('a'))) { it.size }
