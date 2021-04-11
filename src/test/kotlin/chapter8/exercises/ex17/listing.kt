58fee63586ce80811d21493e2e08503242b3ab54
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-11 13:50:08 +0200

    Initial version.

---
+++ src/test/kotlin/chapter8/exercises/ex17/listing.kt
@@ -0,0 +1,14 @@
+/**
+ * Come up with some other properties that takeWhile should satisfy. Can
+ * you think of a good property expressing the relationship between
+ * takeWhile and dropWhile?
+ */
+package chapter8.solutions.ex17
+
+import utils.SOLUTION_HERE
+
+fun main() {
+    //tag::list[]
+    SOLUTION_HERE()
+    //end::list[]
+}
