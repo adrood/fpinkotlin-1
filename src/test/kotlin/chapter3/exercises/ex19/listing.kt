f42d18364e3cc71054a93ec00fec572e2484af3a
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-12 20:47:17 +0200

    Comments added. Expanded the exercise to match with the given solution.

--- src/test/kotlin/chapter3/exercises/ex19/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex19/listing.kt
@@ -1,4 +1,10 @@
 /**
+ * Write functions flatMap and flatMap2 that work like map except that
+ * the function will return a list instead of a single result, and that
+ * list should be inserted into the final resulting list.
+ * flatMap uses both foldRight and append.
+ * flatMap2 only uses foldRight.
+ *
  * Tip: Use a combination of existing functions that we have already
  * defined.
  */
@@ -16,6 +22,10 @@ import utils.SOLUTION_HERE
 fun <A, B> flatMap(xa: List<A>, f: (A) -> List<B>): List<B> =
 
     SOLUTION_HERE()
+
+fun <A, B> flatMap2(xa: List<A>, f: (A) -> List<B>): List<B> =
+
+    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
@@ -24,6 +34,9 @@ class Exercise19 : WordSpec({
         "!map and flatten a list" {
             val xs = List.of(1, 2, 3)
             flatMap(xs) { i -> List.of(i, i) } shouldBe
+                List.of(1, 1, 2, 2, 3, 3)
+
+            flatMap2(xs) { i -> List.of(i, i) } shouldBe
                 List.of(1, 1, 2, 2, 3, 3)
         }
     }
