a56408dc851634a99649fcb2431905c0b7c4783b
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-15 15:00:51 +0200

    Addition of method findFirst, operating on Strings

--- src/main/kotlin/chapter2/sec2/listing.kt
+++ src/main/kotlin/chapter2/sec2/listing.kt
@@ -1,7 +1,27 @@
 package chapter2.sec2
 
-//tag::init[]
+//tag::init1[]
+fun findFirst(ss: Array<String>, key: String): Int {
+    tailrec fun loop(n: Int): Int =
+        when {
+            // If the end of the loop has been reached without finding
+            // the key, return -1
+            n >= ss.size -> -1 // <1>
+            // If the key is found, return its position
+            ss[n] == key -> n // <2>
+            // Recursively call the function, incrementing the counter
+            else -> loop(n + 1) // <3>
+        }
+    // Initialize the loop with count 0
+    return loop(0) // <4>
+}
+//end::init1[]
+
+// tag::init2[]
+
+// Listing 2.4.
 // Polymorphic function to find an element in an array.
+
 // Operate on an array of A, take a predicate function operating
 // on individual elements of A.
 fun <A> findFirst(xs: Array<A>, p: (A) -> Boolean): Int { // <1>
@@ -10,6 +30,7 @@ fun <A> findFirst(xs: Array<A>, p: (A) -> Boolean): Int { // <1>
             // If the end of the loop has been reached
             // without finding the key, return -1
             n >= xs.size -> -1 // <1>
+            // Apply the predicate function to the array element
             // If the key is found, return its position
             p(xs[n]) -> n // <2>
             // Recursively call the function,
@@ -19,5 +40,4 @@ fun <A> findFirst(xs: Array<A>, p: (A) -> Boolean): Int { // <1>
     // Initialise the loop with count 0.
     return loop(0) // <4>
 }
-//end::init[]
 //end::init2[]
