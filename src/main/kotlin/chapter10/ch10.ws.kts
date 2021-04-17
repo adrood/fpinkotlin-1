80dc14ab71f5ba1f8f385c5190bcad8d0a5daa36
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-17 18:28:47 +0200

    Added code.

--- src/main/kotlin/chapter10/ch10.ws.kts
+++ src/main/kotlin/chapter10/ch10.ws.kts
@@ -1,5 +1,12 @@
 import arrow.core.extensions.list.foldable.foldLeft
 import chapter10.sec1.stringMonoid
+import chapter10.sec6.m
+
+val m1 = mapOf("o1" to mapOf("i1" to 1, "i2" to 2))
+val m2 = mapOf("o1" to mapOf("i3" to 3))
+
+m.combine(m1, m2)
+
 
 val words = listOf<String>("Hic", "Est", "Index")
 
