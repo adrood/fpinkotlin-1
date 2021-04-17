a1627a5ec18d540f0ebdb97e000d3a4d343fb938
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-17 09:28:15 +0200

    Code added.

--- src/main/kotlin/chapter6/chapter6.ws.kts
+++ src/main/kotlin/chapter6/chapter6.ws.kts
@@ -1,26 +1,31 @@
 import chapter6.sec2.SimpleRNG
 import chapter6.sec2.RNG
+import chapter6.sec4.rollDie
 import chapter6.sec6.ns2
 
 //Listing 6.1.
 // Using the Kotlin Random class to demonstrate
 // mutation of internal state.
 
-val rng = kotlin.random.Random
+val rng0 = kotlin.random.Random
 
-rng.nextDouble()
+rng0.nextDouble()
 
-rng.nextDouble()
+rng0.nextDouble()
 
-rng.nextInt()
+rng0.nextInt()
 
-rng.nextInt(10)
+rng0.nextInt(10)
 
-// val rng = SimpleRNG(42)
-// val (n1, rng2) = rng.nextInt()
-// n1
-// val (n2, rng3) = rng2.nextInt()
-// n2
+// Choose an arbitrary value to initialize SimpleRNG
+val rng1 = SimpleRNG(42)
+// Destructure the Piar<Int, RNG> returned from nextInt
+val (n1, rng2) = rng1.nextInt()
+n1
+val (n2, rng3) = rng2.nextInt()
+n2
+
+// val zero = rollDie(SimpleRNG(5))
 
 
 
