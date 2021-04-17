bd7948686cfa4d13cd7cfcf5319b50dc83afe3df
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-17 14:11:30 +0200

    Comments added.

--- src/test/kotlin/chapter8/solutions/ex14/listing.kt
+++ src/test/kotlin/chapter8/solutions/ex14/listing.kt
@@ -17,14 +17,19 @@ fun List<Int>.prepend(i: Int) = listOf(i) + this
 //tag::init[]
 val maxProp = forAll(SGen.listOf(smallInt)) { ns ->
     val nss = ns.sorted()
+    // List may be empty
     nss.isEmpty() or // <1>
+        // List may only have a single element
             (nss.size == 1) or // <2>
             nss.zip(nss.prepend(Int.MIN_VALUE))
                 .foldRight(true, { p, b ->
                     val (pa, pb) = p
+                    // List must be ordered in ascending order
                     b && (pa >= pb)
                 }) and // <3>
+        // List must contain all elements of unsorted list
             nss.containsAll(ns) and // <4>
+        // List may not contain any elements that are not in unsorted list
             !nss.exists { !ns.contains(it) } // <5>
 }
 //end::init[]
