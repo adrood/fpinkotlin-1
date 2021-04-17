24d364fb2b6f5e8c34f021aa398ce05f224ff78b
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-17 07:28:27 +0200

    Code added.

--- src/main/kotlin/chapter6/chapter6.ws.kts
+++ src/main/kotlin/chapter6/chapter6.ws.kts
@@ -2,11 +2,25 @@ import chapter6.sec2.SimpleRNG
 import chapter6.sec2.RNG
 import chapter6.sec6.ns2
 
-val rng = SimpleRNG(42)
-val (n1, rng2) = rng.nextInt()
-n1
-val (n2, rng3) = rng2.nextInt()
-n2
+//Listing 6.1.
+// Using the Kotlin Random class to demonstrate
+// mutation of internal state.
+
+val rng = kotlin.random.Random
+
+rng.nextDouble()
+
+rng.nextDouble()
+
+rng.nextInt()
+
+rng.nextInt(10)
+
+// val rng = SimpleRNG(42)
+// val (n1, rng2) = rng.nextInt()
+// n1
+// val (n2, rng3) = rng2.nextInt()
+// n2
 
 
 
