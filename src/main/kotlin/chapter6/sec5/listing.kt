f4e74281c1d83d511726c5ec9796893a98985aee
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-17 09:27:50 +0200

    Comments and code added.

--- src/main/kotlin/chapter6/sec5/listing.kt
+++ src/main/kotlin/chapter6/sec5/listing.kt
@@ -1,5 +1,7 @@
 package chapter6.sec5
 
+import chapter6.sec1.RNG
+
 //tag::init1[]
 // Listing 6.12.
 // Generalized version of map combinator
@@ -9,8 +11,16 @@ fun <S, A, B> map(
 ): (S) -> Pair<B, S> = TODO()
 //end::init1[]
 
+// Listing 6.13.
+// Generalized State type alias for state transition
+typealias State0<S, A> =  (S) -> Pair<A, S>
+
 //tag::init2[]
 // Listing 6.14.
 // Wrapping the state transition in a data class
 data class State<S, out A>(val run: (S) -> Pair<A, S>)
 //end::init2[]
+
+// Listing 6.15.
+// The Rand type alias updated to use State
+typealias Rand<A> = State<RNG, A>
