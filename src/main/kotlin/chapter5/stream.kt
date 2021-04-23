848f7b42378ca44c9a5b9cba80eaa0392fb1983a
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-23 12:24:00 +0200

    Comments added.

--- src/main/kotlin/chapter5/stream.kt
+++ src/main/kotlin/chapter5/stream.kt
@@ -7,19 +7,23 @@ sealed class Stream<out A> {
 
     companion object {
 
+        // smart constructor
         fun <A> cons(hd: () -> A, tl: () -> Stream<A>): Stream<A> {
             val head: A by lazy(hd)
             val tail: Stream<A> by lazy(tl)
             return Cons({ head }, { tail })
         }
 
+        // smart constructors empty() and cons() are used here:
         fun <A> of(vararg xs: A): Stream<A> =
             if (xs.isEmpty()) empty()
             else cons({ xs[0] },
                 { of(*xs.sliceArray(1 until xs.size)) })
 
+        // smart constructor
         fun <A> empty(): Stream<A> = Empty
 
+        // smart constructor
         fun <A> continually(a: A): Stream<A> =
             cons({ a }, { continually(a) })
 
