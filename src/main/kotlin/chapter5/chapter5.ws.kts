20a6b1a89558ccab4338dd92506ff825151445d4
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-23 12:08:16 +0200

    Added code.

--- src/main/kotlin/chapter5/chapter5.ws.kts
+++ src/main/kotlin/chapter5/chapter5.ws.kts
@@ -1,17 +1,25 @@
 import chapter5.sec1.a
 import chapter5.sec1.lazyIf
 import chapter5.sec1.maybeTwice
+import chapter5.sec1.maybeTwice2
 import chapter5.sec1.useit
 
 // Apparently Kotlin doesn't have a List.of method
 //List.of(1, 2, 3, 4)
 
+// Section 1
 // does not print anything
 false && { println("!!"); true }.invoke()
 
 // does not print anything either
 true || { println("!!"); false }.invoke()
 
-maybeTwice(true, { println("hi"); 1 + 41})
+println("Hello")
 
-useit()
+maybeTwice2(true, { println("hi"); 1 + 41})
+
+println("Hello after")
+
+//val x = maybeTwice2(true, { println("hi"); 1 + 41 })
+
+//useit()
