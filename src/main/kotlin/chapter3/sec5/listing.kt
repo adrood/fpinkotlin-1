d52093c646c7c59e592afe3fe68060ec5139eafe
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-24 11:58:59 +0200

    Cosmetic changes

--- src/main/kotlin/chapter3/sec5/listing.kt
+++ src/main/kotlin/chapter3/sec5/listing.kt
@@ -2,8 +2,10 @@ package chapter3.sec5
 
 //Tree structures
 //tag::init1[]
+
 // Listing 3.11.
 // Definition of a binary tree data structure
+
 sealed class Tree<out A>
 
 data class Leaf<A>(val value: A) : Tree<A>()
