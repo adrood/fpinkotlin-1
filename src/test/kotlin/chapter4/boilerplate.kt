38ada008f51cb8c148b684d4d7e57af2ba41bab5
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-26 09:20:02 +0200

    Comments added.

--- src/test/kotlin/chapter4/boilerplate.kt
+++ src/test/kotlin/chapter4/boilerplate.kt
@@ -4,28 +4,40 @@ import chapter3.Cons
 import chapter3.List
 import chapter3.Nil
 
+// Extension function
+// map in terms of foldRight
 fun <A, B> List<A>.map(f: (A) -> B): List<B> =
     this.foldRight(
         List.empty(),
         { a, b -> Cons(f(a), b) })
 
+// Extension function
+// Pattern matching
 fun <A, B> List<A>.foldRight(z: B, f: (A, B) -> B): B =
     when (this) {
         is Nil -> z
         is Cons -> f(this.head, this.tail.foldRight(z, f))
     }
 
+// Extension function
+// Sum in terms of foldRight
+// foldRight is a generalization of sum
 fun List<Double>.sum(): Double =
     this.foldRight(0.0, { a, b -> a + b })
 
+// Extension function
+// Size in terms of foldRight
 fun <A> List<A>.size(): Int =
     this.foldRight(0, { _, acc -> 1 + acc })
 
+// Extension function
+// Pattern matching
 fun List<Double>.isEmpty(): Boolean = when (this) {
     is Nil -> true
     is Cons -> false
 }
 
+// map2 in terms of flatMap and map
 fun <A, B, C> map2(
     oa: Option<A>,
     ob: Option<B>,
@@ -37,24 +49,31 @@ fun <A, B, C> map2(
         }
     }
 
+// Extension function
+// Pattern matching
 fun <E, A, B> Either<E, A>.map(f: (A) -> B): Either<E, B> =
     when (this) {
         is Left -> this
         is Right -> Right(f(this.value))
     }
 
+// Extension function
+// Pattern matching
 fun <E, A> Either<E, A>.orElse(f: () -> Either<E, A>): Either<E, A> =
     when (this) {
         is Left -> f()
         is Right -> this
     }
 
+// Extension function
+// Pattern matching
 fun <E, A, B> Either<E, A>.flatMap(f: (A) -> Either<E, B>): Either<E, B> =
     when (this) {
         is Left -> this
         is Right -> f(this.value)
     }
 
+// map2 in terms of flatMap and map
 fun <E, A, B, C> map2(
     ae: Either<E, A>,
     be: Either<E, B>,
