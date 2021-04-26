be9dfa2f826c2aa9eb5a0c6762864b07c5ec7adc
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-26 10:32:57 +0200

    Improved comment.

--- src/main/kotlin/chapter6/sec1/listing.kt
+++ src/main/kotlin/chapter6/sec1/listing.kt
@@ -5,8 +5,11 @@ import kotlin.random.Random
 
 //tag::init[]
 // Listing 6.2.
-// A method simulating the roll of a die with an off-by-one error.
-// Method should return a random number from 1 to 6
+
+/**
+ * A method simulating the roll of a die with an off-by-one error.
+ * Method should return a random number from 1 to 6
+ */
 fun rollDie(): Int { // <1>
     // Creates a new random number generator seeded with
     // the current system time
